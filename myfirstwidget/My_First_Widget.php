43581316f20e66b46213cee5cc78a09f18b11d4b
Johan Nordström <johan@digitalvillage.se>
2018-05-28 12:04:16 +0200

    Lösningsförslag för dag 2's workshop, uppgift 3 och 4.

--- myfirstwidget/My_First_Widget.php
+++ myfirstwidget/My_First_Widget.php
@@ -27,13 +27,23 @@ class My_First_Widget extends WP_Widget {
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
 		echo $args['before_widget'];
+
+		// widget title
 		if ( ! empty( $title ) ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-		// echo __( 'Hello, Earth!', 'text_domain' );
-		if (!empty($instance['button_text'])) {
+
+		// textarea
+		if (!empty($instance['textarea'])) {
 			?>
-				<button href="#"><?php echo $instance['button_text']; ?></button>
+				<i><?php echo wpautop($instance['textarea']); ?></i>
+			<?php
+		}
+
+		// button text
+		if (!empty($instance['button_text']) && $instance['show_link']) {
+			?>
+				<a href="<?php echo $instance['button_link']; ?>" class="btn btn-primary" role="button"><?php echo $instance['button_text']; ?></a>
 			<?php
 		}
 		echo $args['after_widget'];
@@ -54,7 +64,10 @@ class My_First_Widget extends WP_Widget {
 			$title = __( 'New Heading', 'text_domain' );
 		}
 
+		$textarea = (!empty($instance['textarea']) ? $instance['textarea'] : "");
+		$show_link = (!empty($instance['show_link']) ? $instance['show_link'] : "");
 		$button_text = (!empty($instance['button_text']) ? $instance['button_text'] : "");
+		$button_link = (!empty($instance['button_link']) ? $instance['button_link'] : "");
 
 		?>
 		<!-- title -->
@@ -72,6 +85,35 @@ class My_First_Widget extends WP_Widget {
 			/>
 		</p>
 
+		<!-- textarea -->
+		<p>
+			<label for="<?php echo $this->get_field_name( 'textarea' ); ?>">
+				<?php _e( 'Textarea:' ); ?>
+			</label>
+
+			<textarea
+				cols=30
+				rows=8
+				class="widefat"
+				id="<?php echo $this->get_field_id( 'textarea' ); ?>"
+				name="<?php echo $this->get_field_name( 'textarea' ); ?>"><?php echo esc_attr( $textarea ); ?></textarea>
+		</p>
+
+		<!-- show link? -->
+		<p>
+			<label for="<?php echo $this->get_field_name( 'show_link' ); ?>">
+				<?php _e( 'Show link:' ); ?>
+			</label>
+
+			<input
+				class="widefat"
+				id="<?php echo $this->get_field_id( 'show_link' ); ?>"
+				name="<?php echo $this->get_field_name( 'show_link' ); ?>"
+				type="checkbox"
+				<?php if ($show_link) { ?> checked <?php } ?>
+			/>
+		</p>
+
 		<!-- button text -->
 		<p>
 			<label for="<?php echo $this->get_field_name( 'button_text' ); ?>">
@@ -84,6 +126,21 @@ class My_First_Widget extends WP_Widget {
 				name="<?php echo $this->get_field_name( 'button_text' ); ?>"
 				type="text"
 				value="<?php echo esc_attr( $button_text ); ?>"
+			/>
+		</p>
+
+		<!-- button link -->
+		<p>
+			<label for="<?php echo $this->get_field_name( 'button_link' ); ?>">
+				<?php _e( 'Button link:' ); ?>
+			</label>
+
+			<input
+				class="widefat"
+				id="<?php echo $this->get_field_id( 'button_link' ); ?>"
+				name="<?php echo $this->get_field_name( 'button_link' ); ?>"
+				type="text"
+				value="<?php echo esc_attr( $button_link ); ?>"
 			/>
 		</p>
 
@@ -103,7 +160,10 @@ class My_First_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-		$instance['button_text'] = $new_instance['button_text'];
+		$instance['textarea'] = strip_tags($new_instance['textarea']);
+		$instance['show_link'] = $new_instance['show_link'];
+		$instance['button_text'] = strip_tags($new_instance['button_text']);
+		$instance['button_link'] = strip_tags($new_instance['button_link']);
 
 		return $instance;
 	}
